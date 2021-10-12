5f75e1fd3763855f33674332d1db098966d29407
Kinayat Abay <74435594+AbayKinayat@users.noreply.github.com>
2021-10-13 00:56:00 +0600

    personal-area-layout

---
+++ src/views/PersonalArea.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <h1 class="section-title">Личный кабинет</h1>
+    <div class="personal-area__content">
+      <UserCard />
+    </div>
+  </div>
+</template>
+
+<script>
+import UserCard from "@/components/UserCard";
+export default {
+  name: "PersonalArea",
+  components: {
+    UserCard,
+  },
+};
+</script>
+
+<style>
+.personal-area__content {
+  display: flex;
+  justify-content: space-between;
+  max-width: 990px;
+  width: 100%;
+}
+</style>
